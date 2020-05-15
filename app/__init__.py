4fd5b64f25f72d126fe273c420f2972f3bfcb0b8
SammyJoskey <55281833+SammyJoskey@users.noreply.github.com>
2020-05-15 23:54:34 +1000

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+from .config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+bcrypt = Bcrypt(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+Bootstrap(app)
+
+from app import routes, models

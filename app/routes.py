8231390764506bbff2fd9397293f12fecf26f074
SammyJoskey <55281833+SammyJoskey@users.noreply.github.com>
2020-05-18 14:51:07 +1000

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,109 +1,112 @@
-  
-from datetime import datetime
-import functools
-from flask import jsonify, render_template, request, redirect, flash, url_for, session
+from app import app, login_manager, db, bcrypt
+from .models import *
+from .forms import *
+from flask import request, redirect, render_template, flash
+from flask_login import login_user, logout_user, current_user, login_required
+import datetime
+import logging
 
-from app import app, db, login_manager, bcrypt
-from .models import Event, User
-from .forms import EventForm, LoginForm
+
+@app.route("/", methods=["GET", "POST"])
+def scheduler():
+    events = Event.query.order_by(Event._id).all()
+    return render_template('scheduler.html', events=events, current_user=current_user)
 
 
+# Login #
 @login_manager.user_loader
 def user_loader(user_id):
     return User.query.get(user_id)
 
-def login_required(func):
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        if "email" not in session:
-            return redirect(url_for("login", next=request.url))
-        return func(*args, **kwargs)
-    return secure_function
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = CreateUserForm()
+    if form.validate_on_submit():
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User(email=email, username=username, password=bcrypt.generate_password_hash(password).decode('utf-8'))
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/login")
+    return render_template("register.html", form=form)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.get(form.email.data)
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
-                session["email"] = form.email.data
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user, remember=True)
                 return redirect("/")
     return render_template("login.html", form=form)
 
 
-@app.route("/create_user", methods=["GET", "POST"])
-def create_user():
-    form = LoginForm()
-    if form.validate_on_submit():
-        email = request.form.get('email')
-        password = request.form.get('password')
-        user = User(email=email, password=bcrypt.generate_password_hash(password).decode('utf-8'))
-        db.session.add(user)
-        db.session.commit()
-        return redirect("/")
-    return render_template("create.html", form=form)
-
-
-
-@app.route('/')
-@app.route('/schedule', methods=["GET", "POST"])
+@app.route('/logout')
 @login_required
-def schedule():
-    events = db.session.query(Event).order_by(Event.start.desc()).all()
-    return render_template('schedule.html', events=events)
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    flash('До новых встреч!')
+    return redirect("/")
 
 
-@app.route('/event', methods=['POST', 'GET'])
-@login_required
-def event():
-    event_form = EventForm()
+# Events
+@app.route('/add', methods=["GET", "POST"])
+def add_event():
+    form = EventForm()
     if request.method == 'POST':
-        if event_form.validate_on_submit():
-            author = session['email']
-            start = request.form.get('start')
-            start_format = datetime.strptime(start, '%Y-%m-%d %H:%M')
-            end = request.form.get('end')
-            end_format = datetime.strptime(end, '%Y-%m-%d %H:%M')
+        if form.validate_on_submit():
             subject = request.form.get('subject')
             description = request.form.get('description')
+            date_start = request.form.get('date_start')
+            date_end = request.form.get('date_end')
             event = Event(
-                author=author, 
-                start=start_format, 
-                end=end_format, 
-                subject=subject, 
-                description=description)
+                subject=subject,
+                description=description,
+                date_start=date_start,
+                date_end=date_end,
+                author=current_user.get_id())
             db.session.add(event)
             db.session.commit()
-            return redirect('/')
-        error = "Form was not validated"
-        return render_template('error.html',form=event_form, error = error)        
-    return render_template('add_event.html', form=event_form)
+            return redirect("/")
+    return render_template("add_event.html", form=form, date_start=datetime.datetime.now())
 
-@app.route('/event/<int:event_id>', methods=['GET', 'POST'])
-@login_required
-def event_update(event_id):
-    event = db.session.query(Event).filter(Event._id==int(event_id)).first()
-    event_form = EventForm(obj=event)
+
+@app.route('/edit/<int:_id>', methods=["GET", "POST"])
+def edit_event(_id):
+    event = Event.query.filter(Event._id == int(_id)).first()
+    form = EventForm(obj=event)
     if request.method == 'POST':
-        if event and event_form.validate(): 
-                author = request.form.get('author')
-                if author != session['email']:
-                    error = "Forbidden event to edit"
-                    return render_template('error.html',form=event_form, error = error)
-                start = request.form.get('start')
-                start_format = datetime.strptime(start, '%Y-%m-%d %H:%M')
-                end = request.form.get('end')
-                end_format = datetime.strptime(end, '%Y-%m-%d %H:%M')
-                subject = request.form.get('subject')
-                description = request.form.get('description')
-                event.author = author
-                event.subject = subject
-                event.description = description
-                event.start = start_format
-                event.end = end_format
-                db.session.commit()
-                return redirect('/')
-        error = "Form was not validated"
-        return render_template('error.html',form=event_form, error = error)        
-    return render_template('edit_event.html', form=event_form)
+        if form.validate_on_submit():
+            subject = request.form.get('subject')
+            description = request.form.get('description')
+            date_start = request.form.get('date_start')
+            date_end = request.form.get('date_end')
+            event.subject = subject
+            event.description = description
+            event.date_start = date_start
+            event.date_end = date_end
+            event.author = current_user.get_id()
+            db.session.commit()
+        return redirect("/")
+    return render_template("add_event.html", form=form, date_start=datetime.datetime.now())
+
+
+@app.route('/delete/<int:_id>', methods=["GET", "POST"])
+def del_event(_id):
+    if request.method == 'POST':
+        event = Event.query.filter(Event._id == int(_id)).first()
+        db.session.delete(event)
+        db.session.commit()
+    return redirect("/")

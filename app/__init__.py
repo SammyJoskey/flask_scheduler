e9eec3eec1de7d503e25bdc67eec51a95a0e3d47
SammyJoskey <55281833+SammyJoskey@users.noreply.github.com>
2020-05-18 14:52:49 +1000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
-from .config import Config
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)

da4070bcc73b4bf59b257c4784ef54226966c416
SammyJoskey <55281833+SammyJoskey@users.noreply.github.com>
2020-05-18 14:52:07 +1000

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,30 @@
-from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField
-from wtforms.fields.html5 import DateTimeLocalField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.fields.html5 import EmailField, DateTimeField
+from wtforms.validators import InputRequired, DataRequired, Email, EqualTo
+import datetime
 
-class EventForm(FlaskForm):
-    _id = HiddenField('_id')
-    author = StringField('author')
-    start = DateTimeLocalField('start', format='%Y-%m-%d %H:%M', validators=[DataRequired()])
-    end = DateTimeLocalField('end', format='%Y-%m-%d %H:%M', validators=[DataRequired()])
-    subject = StringField('subject', validators=[DataRequired()])
-    description = StringField('description')
+class CreateUserForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[InputRequired(), DataRequired()])
+    email = EmailField('Электронная почта', validators=[InputRequired(), DataRequired(), Email()])
+    password = PasswordField("Введите пароль", validators=[InputRequired(), DataRequired()])
+
+    submit = SubmitField('Зарегистрироваться')
+
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    email = EmailField('Электронная почта', validators=[InputRequired(), DataRequired(), Email()])
+    password = PasswordField("Пароль", validators=[InputRequired(), DataRequired()])
+
+    submit = SubmitField('Войти')
+
+
+class EventForm(FlaskForm):
+    date_start = DateTimeField("Начало", default=datetime.datetime.now(), format='%Y-%m-%d %H:%M:%S',
+                               validators=[DataRequired(message="Неправильный формат даты")], )
+    date_end = DateTimeField("Окончание", default=datetime.date.today(), format='%Y-%m-%d %H:%M:%S',
+                             validators=[DataRequired(message="Неправильный формат даты")], )
+    subject = StringField("Тема", validators=[InputRequired(), DataRequired()])
+    description = StringField("Описание", validators=[DataRequired()])
+
+    submit = SubmitField('Сохранить')
